81ad06d6d8ec79bd7445db5fda449018ccb6d41d
Grant Miller <grant@grant-miller.com>
2021-11-18 19:34:13 -0500

    added button push

--- flask_raspi_gpio_control/__init__.py
+++ flask_raspi_gpio_control/__init__.py
@@ -2,7 +2,8 @@ import time
 import requests
 import sys
 
-ALL_PIN_NUMBERS = [14, 15, 18, 23, 24, 25, 8, 7, 12, 16, 20, 21, 2, 3, 4, 17, 27, 22, 10, 9, 11, 5, 6, 13, 19, 26]
+ALL_OUTPUT_PIN_NUMBERS = [16, 20, 21, 5, 6, 13, 19, 26]
+PIN_BUTTON = 12
 
 if sys.platform.startswith('win'):
     BASE_URL = 'http://192.168.68.105:5000/'
@@ -16,8 +17,23 @@ else:  # linux
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(True)
 
-for pinName in ALL_PIN_NUMBERS:
+for pinName in ALL_OUTPUT_PIN_NUMBERS:
     GPIO.setup(int(pinName), GPIO.OUT)
+
+GPIO.setup(PIN_BUTTON, GPIO.IN)
+
+
+def BlinkAllLights(numberOfBlinks=1):
+    for i in range(numberOfBlinks * 2):
+        for pin in ALL_OUTPUT_PIN_NUMBERS:
+            GPIO.output(pin, 1)
+        time.sleep(0.2)
+        for pin in ALL_OUTPUT_PIN_NUMBERS:
+            GPIO.output(pin, 0)
+        time.sleep(0.2)
+
+
+BlinkAllLights(25)
 
 
 def Start():
@@ -34,12 +50,15 @@ def Start():
                 {'On': 0, 'Off': 1}.get(state)
             )
 
-        print('Average req/second=', round(totalRequests/(time.time()-startTime), 2))
+        print('Average req/second=', round(totalRequests / (time.time() - startTime), 2))
         if sys.platform.startswith('win'):
             time.sleep(1)
         else:
             # with no delay, raspi was able to send 5 request per second to local webserver
             time.sleep(1)
+
+        if GPIO.input(PIN_BUTTON) == GPIO.LOW:
+            BlinkAllLights(25)
 
 
 if __name__ == '__main__':
